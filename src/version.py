b3128836e71f110c4d6254c00986554b5e4b8774
Nayla Hanegan <NaylaHanegan@gmail.com>
2025-04-22 03:27:19 -0400

    d

--- src/version.py
+++ src/version.py
@@ -1,8 +1,1 @@
-# ============================================
-# CodeFusion
-# Author: Tabitha Hanegan (naylahanegan@gmail.com)
-# Date: 1/4/2025
-# License: MIT
-# ===========================================
-
 appVersion = "1.0.0"
